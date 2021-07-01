11e2d1a487dfa844921fc1cde32d0f85b4ed3632
Omar Munoz <omar.munoz@sudo.cl>
2021-07-01 13:08:36 -0400

    Sube bot con cosas basicas

---
+++ main.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const { DiscordTogether } = require('discord-together');
+require('dotenv').config()
+
+client.discordTogether = new DiscordTogether(client);
+
+client.on('ready', () => {
+	console.log(`Ready as: ${client.user.tag}!`);
+});
+
+client.on('message', async message => {
+    if (message.content === '&help') {
+        return message.channel.send(`&watch2gether => Crea sala para ver videos en youtube`);
+    };
+
+    
+    if (message.content.includes('paja')) {
+        return message.channel.send(message.author.toString(), {files: ["https://cdn.discordapp.com/attachments/692819163458175057/860017361850859520/unknown.png"]});
+    };
+
+    if (message.content === '&watch2gether') {
+        if(message.member.voice.channel) {
+            client.discordTogether.createTogetherCode(message.member.voice.channelID, 'youtube').then(async invite => {
+                return message.channel.send(`Click aqui para crear/unirse ${invite.code}`);
+            });
+        };
+    };
+});
+
+client.login(process.env.TOKEN);
